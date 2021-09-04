7d80fe0635507abc335a8223d6b211db8f9da5bb
KaboomPhysicist <cristian.serna1@udea.edu.co>
2021-09-03 22:34:40 -0500

    Primeras gráficas y animacion simultanea

--- Tarea 3/system_animation.py
+++ Tarea 3/system_animation.py
@@ -6,7 +6,7 @@ from scipy.integrate import odeint
 
 def solve_system(x0,y0,px0,py0,t0,tf): 
     Y0=[x0,y0,px0,py0] #arreglo con las condiciones iniciales 
-    t=np.linspace(t0,tf,1000000) #tiempo que dura la vaina
+    t=np.linspace(t0,tf,100000) #tiempo que dura la vaina
 
     l=1 #Parámetro lambda 
 
@@ -36,18 +36,25 @@ plt.ylabel("Y [m]")
 
 
 
-sol=solve_system(0,0.1,0.44,0.1,0,500)
-N=len(sol[0])
+sol0 = solve_system(0.1,0,0.1,0.02,0,150)
+sol1 = solve_system(0.12,0,0.1,0.02,0,150)
+sol2 = solve_system(0.14,0,0.1,0.02,0,150)
+sol3 = solve_system(0.16,0,0.1,0.02,0,150)
+sol4 = solve_system(0.18,0,0.1,0.02,0,150
+
+N=len(sol0[0])
 
 global vel
-vel = 25
+vel = 30
 
-X=sol[0]
-Y=sol[1]
+X0,Y0=sol0
+X1,Y1=sol1
+X2,Y2=sol2
+X3,Y3=sol3
+X4,Y4=sol4
 
-
-plt.xlim(1.15*X.min(),1.15*X.max())
-plt.ylim(1.15*X.min(),1.15*X.max())
+plt.xlim(1.15*X0.min(),1.15*X0.max())
+plt.ylim(1.15*Y0.min(),1.15*Y0.max())
 
 plt.gca().set_aspect('equal','box')
 
@@ -59,10 +66,15 @@ def update(num):
     
     return  trayectoria1
 
-masa, = plt.plot(X[0],Y[0],'*',color='crimson',markersize=10)
+masa0, = plt.plot(X0[0],Y0[0],'*',color='crimson',markersize=10)
+masa1, = plt.plot(X1[0],Y1[0],'*',color='crimson',markersize=10)
+masa2, = plt.plot(X2[0],Y2[0],'*',color='crimson',markersize=10)
+masa3, = plt.plot(X3[0],Y3[0],'*',color='crimson',markersize=10)
+masa4, = plt.plot(X4[0],Y4[0],'*',color='crimson',markersize=10)
+
 trayectoria1, = plt.plot(X[0],Y[0],color='crimson')
 
 
 ani = animation.FuncAnimation(fig, update, N//vel, interval=10000/(N//vel), blit=False)
 plt.show()
-ani.save('ani.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+#ani.save('ani001.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
